af99a4255d888ad0eaa653ffedd48fa34596b03f
Shayan2007 <shayan.suhail@gmail.com>
2023-05-22 23:09:55 +0500

    upload file code created

--- fileuploader/myapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from myapp.models import UploadedFile
-
-class FileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UploadedFile
-        fields = ('file',)
