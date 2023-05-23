752fe0f19f1471ab9baf12166666fd84ea66d68e
Shayan2007 <shayan.suhail@gmail.com>
2023-05-23 10:41:26 +0500

    upload file code created

--- file_upload_api/myapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import UploadedFile
-
-class UploadedFileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UploadedFile
-        fields = '__all__'
--- file_upload_api/myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UploadedFileViewSet
-
-urlpatterns = [
-    path('uploaded-files/', UploadedFileViewSet.as_view({'post': 'create'}), name='uploadedfile-list'),
-]
